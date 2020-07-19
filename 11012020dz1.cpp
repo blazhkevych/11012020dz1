3afa8a96a9e0467e09c1f5b639b559bc3ab67e98
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:03:42 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz1.cpp
@@ -0,0 +1,15 @@
+﻿/*
+ Пользователь 	вводит с клавиатуры две стороны 	прямоугольника.Посчитать площадь и 	периметр прямоугольника.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+
+int main()
+{
+	int a, b, P, S;
+	cin >> a >> b;
+	P = 2 * a + 2 * b;
+	S = a * b;
+	cout << P << "\n" << S << endl;
+}
